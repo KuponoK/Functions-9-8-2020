cf81c33e7120419fc03036e3a0e8fc8bd88cc3b5
Kupono_Kealiinohomoku <kupono.kealiinohomoku@gmail.com>
2020-09-08 22:02:12 -0700

    first commit

---
+++ functions.py
@@ -0,0 +1,32 @@
+def area(width, height):
+    result = width * height
+    return result
+
+num_1 = area(5, 6)
+print(num_1)
+num_2 = area(4, 12)
+print(num_2)
+num_3 = area(5, 8)
+print(num_3)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+num_4 = subtract(3, 2)
+print(num_4)
+num_5 = subtract(6, 3)
+print(num_5)
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+num_6 = add(2, 2)
+print(num_6)
+num_7 = add(3, 3)
+print(num_7)
